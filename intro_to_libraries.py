0dc73c33bedbd2f84727464ae4bb33bad42380f5
Parichat <jungjinggg@icloud.com>
2023-06-15 13:23:06 +0100

    first commit

---
+++ intro_to_libraries.py
@@ -0,0 +1,16 @@
+"""
+Libraries
+    collections of function (pre-built code) that do a job for you.
+    we can import these libraries to make our code easier to understand, be less complex and faster to write
+"""
+
+# from random import randint, random
+# import math
+#
+# # print(randint(1, 100))
+# print(random())
+#
+# num_float = 23.66
+# print(math.ceil(num_float))
+# print(math.floor(num_float))
+# print(math.pi)
